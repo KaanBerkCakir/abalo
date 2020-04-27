40b6be20085d9fa7f91e36d997b9540be0e6ac22
Sanoj3008 <jonas.schell@gmx.de>
2020-04-27 12:08:52 +0200

    categoryController added

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('category')->group(function () {
+    Route::get('all', 'CategoryController@getAll');
+});
