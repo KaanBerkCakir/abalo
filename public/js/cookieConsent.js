200aee31e6407192446993d230c0df8d1d5f3817
Sanoj3008 <jonas.schell@gmx.de>
2020-05-01 20:58:50 +0200

    cookie consent included

--- public/js/cookieConsent.js
+++ public/js/cookieConsent.js
@@ -1,26 +1,36 @@
 function startCookieConsent() {
-    const keks = localStorage.getItem('cookieconsent');
-    if (!keks) {
-        document.body.innerHTML += '\ <div id="cookieConsent">Mit dem klicken auf dieses Banner, stimmen Sie zu das die Seite Cookies benutzen darf</div>\ ';
-        document.getElementById("cookieConsent").addEventListener("click", consenting);
-    }
-}
+    const box = document.createElement('div');
+    box.id = 'cookieConsent';
+    box.classList = 'row jc-center al-center';
 
-function consenting() {
-    const keks = localStorage.getItem('cookieconsent');
-    if (!keks) {
-        console.log("Initialisiere");
-    }
-    alert("funktioniert");
-    localStorage.setItem('cookieconsent', true);
-    console.log(localStorage.getItem('cookieconsent'));
+    const msg = document.createElement('span');
+    msg.innerText = 'Erlaubst du abalo die Verwendung von Cookies?';
+
+    const yes = document.createElement('button');
+    yes.onclick =  () => {
+        localStorage.setItem('cookieconsent', true);
+        document.body.removeChild(box);
+    };
+    yes.innerText = 'Einverstanden';
+
+    const no = document.createElement('button');
+    no.onclick =  () => {
+        localStorage.setItem('cookieconsent', false);
+        document.body.removeChild(box);
+    };
+    no.innerText = 'Nein, danke!';
+
+    box.appendChild(msg);
+    box.appendChild(yes);
+    box.appendChild(no);
+    document.body.appendChild(box);
 }
 
 function isConsentGiven() {
     const keks = localStorage.getItem('cookieconsent');
-    if (keks) {
-        return true;
-    } else {
+    console.log(keks);
+    if (keks === null) {
+        startCookieConsent();
         return false;
-    }
+    } else return keks==='true';
 }
